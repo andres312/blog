67823690cc9955415abb2cc2221675a649f4a073
Andres Rodriguez <andresroga@gmail.com>
2021-10-22 10:12:40 -0500

    Added Slugagable. added bootstrap with auth

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Cviebrock\EloquentSluggable\Sluggable;
 class Post extends Model
 {
     use HasFactory;
+    use Sluggable;
+
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        // when saved get title field as sluggable
+        return [
+            'slug' => [
+                'source' => 'title',
+                'onUpdate' => true
+            ]
+        ];
+    }
 }
